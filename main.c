a60b0030c2cf8e1092836f09215244b45c4dcbdb
AnnaSidorovaMEPHI <Anna.Sidorova.1998@yandex.ru>
2017-04-06 01:43:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ int main(){
     void *M;
     test1(&v1,&v2,&v3,&M);
     test2(&v4,&v5,&v6,&M);
-    //test3(&v7,&v8,&v9);
     }    else{
         printf("The number of what type will contain vectors? ");
         printf("\n");
@@ -33,9 +32,6 @@ int main(){
             M=0;
             multiplication(&v1,&v2, multiplication_int, &M);
             printf("%d", M);
-            /*free(&v1);
-            free(&v2);
-            free(&v3);*/
             }
            else if (c==2){
                 vvod(&v1,vvod_float,sizeof(float));
@@ -49,9 +45,6 @@ int main(){
                 M=0;
                 multiplication(&v1,&v2,multiplication_float, &M);
                 printf("%f", M);
-                /*free(&v1);
-                free(&v2);
-                free(&v3);*/
             }else if (c==3){
                 vvod(&v1,vvod_complex,sizeof(float));
                 print(&v1,print_complex,sizeof(float));
@@ -65,9 +58,6 @@ int main(){
                 M.y=0;
                 multiplication(&v1,&v2,multiplication_complex, &M);
                 printf("{ %f +i*%f}", M.x, M.y);
-                /*free(&v1);
-                free(&v2);
-                free(&v3);*/
             }else
                     printf("ERROR! You entered does not exist the type of the variables!");
                 }
